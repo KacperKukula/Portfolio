f5b0c3da7eb6d4ca04605a7ea47251b78adbb1bb
KacperKukula <50201673+KacperKukula@users.noreply.github.com>
2022-05-25 20:03:39 +0200

    \ Add AnimStart.css and index.php, plain

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="StartAnimation.css">
+
+    <title>Kacper Kukula</title>
+</head>
+<body>
+    <div id="start-anim">
+        <div id="start-anim-main">
+            Witaj
+        </div>
+    </div>
+    
+</body>
+</html>
