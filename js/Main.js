5ed95e7d3e3e325764218a86c4e1e2b51f5f7826
unknown <kukulakacper@outlook.com>
2022-08-11 15:46:32 +0200

    +Slide while changing site

--- js/Main.js
+++ js/Main.js
@@ -18,8 +18,10 @@ gsap.set(main, {yPercent: 100})
 //SPA
 for(let i = 0; i < 4; i++) {
     NavLinker[i].addEventListener("click", function(e) {
+
+        //Eject main content
         gsap.to(main, 1.5, {yPercent: 0, ease: "power4.out"})
-        gsap.to(nav, 1.5, {yPercent: 0})
+        gsap.to(nav, 1.5, {yPercent: 0})    
 
         ///Spa
         switch(i) {
