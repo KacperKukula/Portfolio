25a356e7029bd8a202e3f3d3bc07b84d647daf9c
KacperKukula <50201673+KacperKukula@users.noreply.github.com>
2022-08-25 22:12:25 +0200

    My branch (#6)
    * +Slide while changing site
    
    * Added contact site, ul data (not linket)
    
    * +icons directory
    
    * branch test rebase
    
    * +Slide while changing site
    
    * Added contact site, ul data (not linket)
    
    * +projects.html and some work on it
    
    * project.html 1/2 In progress
    
    * project.html 2/2 Complited
    
    * Done (no RWD) +footer, +sites complited
    
    * - performance: list sttyling,
     - projects: filter hover efect,
     - autoprefixed
     - projects: linked css-trick and added pic
    
    Co-authored-by: Kacper PC <kukulakacper@outlook.com2>

--- js/spa.js
+++ js/spa.js
@@ -1,74 +1,79 @@
-// Instruction 
-// makeRequest("[Your url]", Object) 
+// Instruction
+// makeRequest("[Your url]", Object)
 //Task: add callback
 
 function makeRequest(_url, _container) {
-    http_request = false;
+  http_request = false;
 
-    //Ajax from MDN
-    if (window.XMLHttpRequest) { // Mozilla, Safari,...
-        http_request = new XMLHttpRequest();
-        if (http_request.overrideMimeType) {
-            http_request.overrideMimeType('text/html');
-        }
-    } else if (window.ActiveXObject) { // IE
-        try {
-            http_request = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                http_request = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {}
-        }
+  //Ajax from MDN
+  if (window.XMLHttpRequest) {
+    // Mozilla, Safari,...
+    http_request = new XMLHttpRequest();
+    if (http_request.overrideMimeType) {
+      http_request.overrideMimeType("text/html");
     }
-    if (!http_request) {
-        alert('HTTP request faulty');
-        return false;
+  } else if (window.ActiveXObject) {
+    // IE
+    try {
+      http_request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        http_request = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {}
     }
+  }
+  if (!http_request) {
+    alert("HTTP request faulty");
+    return false;
+  }
 
-    //What happen if request okay
-    http_request.onreadystatechange = function() { 
-        if (http_request.readyState == 4) {
-            if(http_request.status==200) {
-                /*//////////////////////////////////////*/
-                if(IsMainEject) {
-                    tl = gsap.timeline()
-                    tl.to(AJAX, 0.4, {opacity: 0, x: -20, onComplete: InnerHTML})
-                        .set(AJAX, {x: 20})
-                        .to(AJAX, 0.4, {opacity: 1, x: 0, onComplete: SwitchAnimation, onCompleteParams: [_url]})
-                }
-                else {
-                    IsMainEject = true
-                    InnerHTML()
-                }
-
-                function InnerHTML() { //callback
-                    _container.innerHTML = http_request.response
-                }
-                /*//////////////////////////////////////*/
-            } else {
-                _container.innerHTML = "something is not right"
-            }
+  //What happen if request okay
+  http_request.onreadystatechange = function () {
+    if (http_request.readyState == 4) {
+      if (http_request.status == 200) {
+        /*//////////////////////////////////////*/
+        if (IsMainEject) {
+          tl = gsap.timeline();
+          tl.to(AJAX, 0.4, { opacity: 0, x: -20, onComplete: InnerHTML })
+            .set(AJAX, { x: 20 })
+            .to(AJAX, 0.4, {
+              opacity: 1,
+              x: 0,
+              onComplete: SwitchAnimation,
+              onCompleteParams: [_url],
+            });
+        } else {
+          IsMainEject = true;
+          InnerHTML();
         }
-    };
 
-    _url = encodeURIComponent(_url);
-    http_request.open('GET', _url, true);
-    http_request.send(null);
+        function InnerHTML() {
+          //callback
+          _container.innerHTML = http_request.response;
+        }
+        /*//////////////////////////////////////*/
+      } else {
+        _container.innerHTML = "something is not right";
+      }
+    }
+  };
+
+  //_url = encodeURIComponent(_url);
+  http_request.open("GET", _url, true);
+  http_request.send(null);
 }
 
 function SwitchAnimation(_url) {
-    switch(_url) {
-        case "content/aboutme.html":
-            gsap.to(document.querySelector(".Image"), 0.5, {delay: 1.5, x: 20, y: 20})
-            gsap.to(document.querySelector(".frame"), 0.5, {delay: 1.5, x: -20, y: -20})
-        case "content/performance.html":
-            break;
-        case "content/none.html":
-            break;
-        case "content/none.html":
-            break;
-        default:
-    }
-    console.log(_url)
-    gsap.to(AJAX, 0.5,{opacity: 1})
+  switch (_url) {
+    case "content/aboutme.html":
+      break;
+    case "content/performance.html":
+      break;
+    case "content/none.html":
+      break;
+    case "content/none.html":
+      break;
+    default:
+  }
+  gsap.to(AJAX, 0.5, { opacity: 1 });
 }
